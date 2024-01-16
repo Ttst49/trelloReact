f347b8ce53193754b2c7c66cbae8ec526b840a5c
Ttst <thibautstachnick@gmail.com>
2024-01-16 20:07:38 +0100

    add a register method add a logout method
    add a isLogged method for templating
    
    add a template that define if there is an actual user

--- src/UserManager.tsx
+++ src/UserManager.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import "bootstrap/dist/css/bootstrap.min.css"
 
@@ -7,20 +7,35 @@ export default function App() {
     const [password, setPassword] = useState('');
 
 
-  useEffect(() => {
-  });
-
   function login(){
-      const item = {username,password};
-      axios.post("https://trello.thibautstachnick.com/api/token",item)
+      const user = {username,password};
+      axios.post("https://trello.thibautstachnick.com/api/token",user)
           .then((response)=>{
               localStorage.setItem("bearerToken",response.data["access"])
-              console.log(response.data["access"])
           })
+      setTimeout(()=>{window.location.reload()},500)
+  }
+
+  function logout(){
+      localStorage.removeItem("bearerToken");
+      window.location.reload();
+  }
+
+  function register(){
+      const user = {username,password};
+      axios.post("http://trello.thibautstachnick.com/api/register",user)
+          .then((response)=>{
+              console.log(response)
+          })
+  }
+
+  function isLogged(){
+      return !!localStorage.getItem("bearerToken");
   }
 
   return (
       <>
+          <a onClick={logout} className="btn btn-danger">Log out</a>
           <h1>Login page</h1>
           <div className="col-sm-6 offset-sm-3 ">
               <input type="text"
@@ -34,10 +49,11 @@ export default function App() {
               className="form-control"/>
 
             <br/>
-              <button onClick={login} className="btn btn-outline-success" type="submit">Login</button>
+              <button onClick={login} className="btn btn-outline-success">Login</button>
 
 
           </div>
+          {isLogged() ? <p>You are logged in</p> : <p>Log you in</p>}
       </>
 
   );
