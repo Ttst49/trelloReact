06e48e6bf8c985350a4d97a086b184fe4c8ea773
Ttst <thibautstachnick@gmail.com>
2024-04-12 18:28:30 +0200

    add a default value and a verification to show workspaces

--- src/workspace/WorkspaceIndexing.tsx
+++ src/workspace/WorkspaceIndexing.tsx
@@ -6,7 +6,7 @@ import {useNavigate} from "react-router-dom";
 
 export function WorkspaceIndexing() {
     const [isLoading, setLoading] = useState(true);
-    const [workspaces , setWorkspaces] = useState();
+    const [workspaces , setWorkspaces] = useState<Workspace[]>([]);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -33,23 +33,24 @@ export function WorkspaceIndexing() {
     }
 
 
-
-    return (
-        <div className="cards">
-            {workspaces.map((workspace: Workspace)=> (
-                <div key={workspace.id} className="card">
-                    <h5>Id={workspace.id}</h5>
-                    <h4><strong>Titre</strong></h4>
-                    <p>{workspace.name}</p>
-                    <h4><strong>Description</strong></h4>
-                    <p><i>{workspace.description}</i></p>
-                    <p className="card-footer">Created by {workspace.owner.username}</p>
-                    <a onClick={()=>removeWorkspace(workspace)} className="btn btn-outline-danger">Supprimer</a>
-                    <a onClick={()=>navigate("/workspace/edit/"+workspace.id)} className="btn btn-outline-warning">Editer ce workspace</a>
-                    <a onClick={()=>navigate("/board/create/"+workspace.id)} className="btn btn-outline-secondary">Ajouter un tableau</a>
-                    <a onClick={()=>navigate("/board/showAll/"+workspace.id)} className="btn btn-outline-dark">Voir les tableaux de ce workspace</a>
-                </div>
-            ))}
-        </div>
-    );
+    if (workspaces){
+        return (
+            <div className="cards">
+                {workspaces.map((workspace: Workspace)=> (
+                    <div key={workspace.id} className="card">
+                        <h5>Id={workspace.id}</h5>
+                        <h4><strong>Titre</strong></h4>
+                        <p>{workspace.name}</p>
+                        <h4><strong>Description</strong></h4>
+                        <p><i>{workspace.description}</i></p>
+                        <p className="card-footer">Created by {workspace.owner.username}</p>
+                        <a onClick={()=>removeWorkspace(workspace)} className="btn btn-outline-danger">Supprimer</a>
+                        <a onClick={()=>navigate("/workspace/edit/"+workspace.id)} className="btn btn-outline-warning">Editer ce workspace</a>
+                        <a onClick={()=>navigate("/board/create/"+workspace.id)} className="btn btn-outline-secondary">Ajouter un tableau</a>
+                        <a onClick={()=>navigate("/board/showAll/"+workspace.id)} className="btn btn-outline-dark">Voir les tableaux de ce workspace</a>
+                    </div>
+                ))}
+            </div>
+        );
+    }
 }
