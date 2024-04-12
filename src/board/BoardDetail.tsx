6b0d9c8de29f196f6b9c20417c5c7091fbdf77a4
Ttst <thibautstachnick@gmail.com>
2024-04-12 18:01:15 +0200

    change template of board detail

--- src/board/BoardDetail.tsx
+++ src/board/BoardDetail.tsx
@@ -5,15 +5,17 @@ import {useEffect, useState} from "react";
 import "../assets/BoardDetail.css"
 import {List} from "../interface/List.ts";
 import {Card} from "../interface/Card.ts";
+import {Board} from "../interface/Board.ts";
 
 
 export function BoardDetail() {
     const [isLoading, setLoading] = useState(true);
-    const [board , setBoard] = useState();
+    const [board , setBoard] = useState<Board>();
     const [cardName, setCardName] = useState("");
     const navigate = useNavigate();
 
 
+    const {id} = useParams();
 
     useEffect(() => {
         axiosHttp.get(GlobalConstants.baseUrl+"board/show/"+id)
@@ -22,7 +24,7 @@ export function BoardDetail() {
                 setLoading(false)
             })
 
-    },[]);
+    },[id]);
 
     async function createCard(listId:string){
         await axiosHttp.post(GlobalConstants.baseUrl+'card/create/'+listId, {"name":cardName})
@@ -34,54 +36,63 @@ export function BoardDetail() {
             })
     }
 
-    const {id} = useParams();
 
 
     if (isLoading) {
         return <div className="App">Loading...</div>;
     }
 
-    return (
-        <>
-            <div className="topBar">
-                <span>{board.name}</span>
-                <span>{board.id}</span>
-            </div>
-            <div className="mainContent">
-                {board.lists.map((list:List) => (
-                    <div key={list.id}>
-                        <div className="listCard">
-                            <div className="topCard">
-                                <span>{list.name}</span>
-                            </div>
-                            <div className="newCard">
-                                <input
-                                    className="cardCreation"
-                                    type="text"
-                                    placeholder="Add card"
-                                    onChange={(e) => setCardName(e.target.value)}
-                                />
-                                <button className={"creationButton"} onClick={()=>{createCard(list.id.toString()).then()}}>
-                                    Add
-                                </button>
-                            </div>
-                            {list.cards.map((card:Card)=> (
-                                <div className="contentCard">
-                                    <ul>
-                                        <li>
-                                            <a className={"cardLink"}>{card.name}</a>
-                                        </li>
-                                    </ul>
+    if (board){
+        return (
+            <>
+                <div className="topBar">
+                    <span>{board.name}</span>
+                    <span>{board.id}</span>
+                </div>
+                <div className="mainContent">
+                    {board.lists.map((list: List) => (
+                        <div key={list.id}>
+                            <div className="listCard">
+                                <div className="topCard">
+                                    <span>{list.name}</span>
                                 </div>
-                            ))}
+                                <div className="newCard">
+                                    <input
+                                        className="cardCreation"
+                                        type="text"
+                                        placeholder="Add card"
+                                        onChange={(e) => setCardName(e.target.value)}
+                                    />
+                                    <button className={"creationButton"} onClick={() => {
+                                        createCard(list.id.toString()).then()
+                                    }}>
+                                        Add
+                                    </button>
+                                </div>
+                                {list.cards.map((card: Card) => (
+                                    <div className="contentCard">
+                                        <ul>
+                                            <li>
+                                                <a
+                                                    href={"/card/edit/"+card.id}
+                                                    className={"cardLink"}
+                                                >
+                                                    {card.name}
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
-                    </div>
-                ))}
-                <button onClick={() => {
-                    navigate("/list/create/" + board.id)
-                }} className="topButton btn btn-primary">Créer une liste
-                </button>
-            </div>
-        </>
-    );
+                    ))}
+                    <button onClick={() => {
+                        navigate("/list/create/" + board.id)
+                    }} className="topButton btn btn-primary">Créer une liste
+                    </button>
+                </div>
+            </>
+        );
+    }
+
 }
